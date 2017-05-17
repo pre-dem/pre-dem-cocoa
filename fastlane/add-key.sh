81ce16de3baa6179f19bb305a30aeee4311ebbe9
cnwangsiyu <cn.wangsiyu@gmail.com>
2017-05-17 15:13:33 +0800

    config env

---
+++ fastlane/add-key.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#  add-key.sh
+#  PreSniffObjcDemo
+#
+#  Created by WangSiyu on 17/05/2017.
+#  Copyright © 2017 pre-engineering. All rights reserved.
+
+# Create a custom keychain
+security create-keychain -p travis ios-build.keychain
+
+# Make the custom keychain default, so xcodebuild will use it for signing
+security default-keychain -s ios-build.keychain
+
+# Unlock the keychain
+security unlock-keychain -p travis ios-build.keychain
+
+# Set keychain timeout to 1 hour for long builds
+# see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
+
+# Add certificates to keychain and allow codesign to access them
+security import ./fastlane/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./fastlane/Certificates.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./fastlane/Certificates.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+
+
+# Put the provisioning profile in place
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+cp "./fastlane/preengineeringPreSniffSDKDemo_InHouse.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
