bee8b6ca3fd092923fac3553ef7f0bc428ff756a
cnwangsiyu <cn.wangsiyu@gmail.com>
2017-05-18 00:24:29 +0800

    update build

---
+++ scripts/decript-cer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  add-key.sh
+#  PreSniffObjcDemo
+#
+#  Created by WangSiyu on 17/05/2017.
+#  Copyright © 2017 pre-engineering. All rights reserved.
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+openssl aes-256-cbc -K $encrypted_6b87064fc261_key -iv $encrypted_6b87064fc261_iv -in scripts/profile/preengineeringPreSniffObjcDemo_InHouse.mobileprovision.enc -out scripts/profile/preengineeringPreSniffObjcDemo_InHouse.mobileprovision -d
+openssl aes-256-cbc -K $encrypted_6b87064fc261_key -iv $encrypted_6b87064fc261_iv -in scripts/certs/dist.cer.enc -out scripts/certs/dist.cer -d
+openssl aes-256-cbc -K $encrypted_6b87064fc261_key -iv $encrypted_6b87064fc261_iv -in scripts/certs/dist.p12.enc -out scripts/certs/dist.p12 -d
+openssl aes-256-cbc -K $encrypted_6b87064fc261_key -iv $encrypted_6b87064fc261_iv -in scripts/certs/apple.cer.enc -out scripts/certs/apple.cer -d
+exit $?
+fi
