f6471574cec7dfc48d00b59141f6e782f594eefc
cnwangsiyu <cn.wangsiyu@gmail.com>
2017-05-22 17:19:06 +0800

    refine

--- scripts/decript-cer.sh
+++ scripts/decript-cer.sh
@@ -6,7 +6,7 @@
 #  Created by WangSiyu on 17/05/2017.
 #  Copyright © 2017 pre-engineering. All rights reserved.
 
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_TAG =~ "^v.*\.rc[1-9]$" ]]; then
 openssl aes-256-cbc -K $encrypted_6b87064fc261_key -iv $encrypted_6b87064fc261_iv -in scripts/profile/preengineeringPreSniffObjcDemo_InHouse.mobileprovision.enc -out scripts/profile/preengineeringPreSniffObjcDemo_InHouse.mobileprovision -d
 openssl aes-256-cbc -K $encrypted_6b87064fc261_key -iv $encrypted_6b87064fc261_iv -in scripts/certs/dist.cer.enc -out scripts/certs/dist.cer -d
 openssl aes-256-cbc -K $encrypted_6b87064fc261_key -iv $encrypted_6b87064fc261_iv -in scripts/certs/dist.p12.enc -out scripts/certs/dist.p12 -d
