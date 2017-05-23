74d713e198b20e344e63b2e6b33852af61f4309f
cnwangsiyu <cn.wangsiyu@gmail.com>
2017-05-23 10:48:45 +0800

    update

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 bundle install
 
-env
+reg_rc="^v[0-9]+\.[0-9]+\.[0-9]+-rc[0-9]*$"
+reg_release="^v[0-9]+\.[0-9]+\.[0-9]+$"
 
 # rc
-if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_TAG =~ "^v.*\.rc[1-9]$" ]]; then
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_TAG =~ $reg_rc ]]; then
   bundle exec fastlane beta
   exit $?
 # release
-elif [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_TAG =~ "^v.*$" ]]; then
+elif [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_TAG =~ $reg_release ]]; then
   bundle exec fastlane release
   exit $?
 # commit or pr
