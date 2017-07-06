8114ba7c7bb8038de02ba470be3951c545dfd838
cnwangsiyu <cn.wangsiyu@gmail.com>
2017-07-06 10:44:48 +0800

    refine

--- PreDemObjc/Core/PREDManagerPrivate.h
+++ PreDemObjc/Core/PREDManagerPrivate.h
@@ -18,79 +18,14 @@
 PREDConfigManagerDelegate
 >
 
-///-----------------------------------------------------------------------------
-/// @name Environment
-///-----------------------------------------------------------------------------
-
-/**
- Enum that indicates what kind of environment the application is installed and running in.
- 
- This property can be used to disable or enable specific funtionality
- only when specific conditions are met.
- That could mean for example, to only enable debug UI elements
- when the app has been installed over PreDem but not in the AppStore.
- 
- The underlying enum type at the moment only specifies values for the AppStore,
- TestFlight and Other. Other summarizes several different distribution methods
- and we might define additional specifc values for other environments in the future.
- 
- @see PREDEnvironment
- */
 @property (nonatomic, readonly) PREDEnvironment appEnvironment;
 
-/**
- Defines the server URL to send data to or request data from
- 
- By default this is set to the PreDem servers and there rarely should be a
- need to modify that.
- Please be aware that the URL for `PREDMetricsManager` needs to be set separately
- as this class uses a different endpoint!
- 
- @warning This property needs to be set before calling `startManager`
- */
 @property (nonatomic, strong) NSString * _Nullable serverURL;
 
-///-----------------------------------------------------------------------------
-/// @name Modules
-///-----------------------------------------------------------------------------
-
-/**
- Reference to the initialized PREDCrashManager module
- 
- Returns the PREDCrashManager instance initialized by PREDManager
- 
- @see configureWithIdentifier:delegate:
- @see configureWithBetaIdentifier:liveIdentifier:delegate:
- @see startManager
- @see disableCrashManager
- */
 @property (nonatomic, strong, readonly) PREDCrashManager * _Nullable crashManager;
 
-
-/**
- Flag the determines whether the Crash Manager should be disabled
- 
- If this flag is enabled, then crash reporting is disabled and no crashes will
- be send.
- 
- Please note that the Crash Manager instance will be initialized anyway, but crash report
- handling (signal and uncaught exception handlers) will **not** be registered.
- 
- @warning This property needs to be set before calling `startManager`
- 
- *Default*: _NO_
- @see crashManager
- */
 @property (nonatomic, getter = isCrashManagerDisabled) BOOL disableCrashManager;
 
-/**
- Flag the determines whether the HttpMonitor should be disabled
- 
- If this flag is enabled, then sending HttpMonitor data
- will be turned off!
- 
- *Default*: _NO_
- */
 @property (nonatomic, getter = isHttpMonitorDisabled) BOOL disableHttpMonitor;
 
 +(PREDManager *_Nonnull)sharedPREDManager;
@@ -98,7 +33,5 @@ PREDConfigManagerDelegate
 -(nonnull NSString*) baseUrl;
 
 @end
-
-//extern PREDConfigManager* _Nonnull g_pred_sharedmanager();
 
 #endif /* PREDManagerPrivate_h */
