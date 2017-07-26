ae934e6cd2d18b09b18f0888d20f3a37aca6b46e
cnwangsiyu <cn.wangsiyu@gmail.com>
2017-07-26 17:18:49 +0800

    add config

--- PreDemObjc/Config/PREDConfig.m
+++ PreDemObjc/Config/PREDConfig.m
@@ -16,6 +16,7 @@
     config.crashReportEnabled = YES;
     config.lagMonitorEnabled = YES;
     config.onDeviceSymbolicationEnabled = YES;
+    config.webviewEnabled = YES;
     return config;
 }
 
@@ -25,6 +26,7 @@
     config.crashReportEnabled = [[dic objectForKey:@"crash_report_enabled"] boolValue];
     config.lagMonitorEnabled = [[dic objectForKey:@"lag_monitor_enabled"] boolValue];
     config.onDeviceSymbolicationEnabled = [[dic objectForKey:@"on_device_symbolication_enabled"] boolValue];
+    config.onDeviceSymbolicationEnabled = [[dic objectForKey:@"webview_enabled"] boolValue];
 
     return config;
 }
