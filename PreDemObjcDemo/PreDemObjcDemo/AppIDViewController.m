9f96b15a9d1ce433ca2f60de8c48c6bb0de9c9ed
SiyuWang <cn.wangsiyu@gmail.com>
2017-08-01 11:27:33 +0800

    add appID input

---
+++ PreDemObjcDemo/PreDemObjcDemo/AppIDViewController.m
@@ -0,0 +1,67 @@
+//
+//  AppIDViewController.m
+//  PreDemObjcDemo
+//
+//  Created by 王思宇 on 8/1/17.
+//  Copyright © 2017 pre-engineering. All rights reserved.
+//
+
+#import "AppIDViewController.h"
+#import <PreDemObjc/PREDemObjc.h>
+
+#define kPreviousAppId  @"kPreviousAppId"
+
+@interface AppIDViewController ()
+
+@property (nonatomic, strong) IBOutlet UITextField *textField;
+
+@end
+
+@implementation AppIDViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSString *prevID = [[NSUserDefaults standardUserDefaults] stringForKey:kPreviousAppId];
+    if (prevID) {
+        _textField.text = prevID;
+    }
+    UITapGestureRecognizer *g = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapRootview:)];
+    [self.view addGestureRecognizer:g];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)tapRootview:(UIView *)view {
+    [_textField resignFirstResponder];
+}
+
+- (BOOL)shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
+    if (_textField.text.length < 8) {
+        [[[UIAlertView alloc] initWithTitle:@"出错啦" message:@"appID必须在8位以上才能继续哦" delegate:nil cancelButtonTitle:@"好的" otherButtonTitles:nil] show];
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    [[NSUserDefaults standardUserDefaults] setObject:_textField.text forKey:kPreviousAppId];
+    [PREDManager startWithAppKey:_textField.text
+                   serviceDomain:@"http://hriygkee.bq.cloudappl.com"];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
