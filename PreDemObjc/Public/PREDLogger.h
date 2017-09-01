5028c219419caf7990b6f650018382d6bec30568
Siyu Wang <cn.wangsiyu@gmail.com>
2017-09-01 12:00:36 +0800

    add tag log

--- PreDemObjc/Public/PREDLogger.h
+++ PreDemObjc/Public/PREDLogger.h
@@ -17,6 +17,12 @@ static const DDLogLevel predLogLevel = DDLogLevelAll;
 #define PREDLogDebug(frmt, ...)   LOG_MAYBE(LOG_ASYNC_ENABLED, predLogLevel, DDLogFlagDebug,   0, nil, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
 #define PREDLogVerbose(frmt, ...) LOG_MAYBE(LOG_ASYNC_ENABLED, predLogLevel, DDLogFlagVerbose, 0, nil, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
 
+#define PREDTagLogError(tag, frmt, ...)   LOG_MAYBE(NO,                predLogLevel, DDLogFlagError,   0, tag, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
+#define PREDTagLogWarn(tag, frmt, ...)    LOG_MAYBE(LOG_ASYNC_ENABLED, predLogLevel, DDLogFlagWarning, 0, tag, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
+#define PREDTagLogInfo(tag, frmt, ...)    LOG_MAYBE(LOG_ASYNC_ENABLED, predLogLevel, DDLogFlagInfo,    0, tag, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
+#define PREDTagLogDebug(tag, frmt, ...)   LOG_MAYBE(LOG_ASYNC_ENABLED, predLogLevel, DDLogFlagDebug,   0, tag, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
+#define PREDTagLogVerbose(tag, frmt, ...) LOG_MAYBE(LOG_ASYNC_ENABLED, predLogLevel, DDLogFlagVerbose, 0, tag, __PRETTY_FUNCTION__, frmt, ##__VA_ARGS__)
+
 
 @interface PREDLogger : NSObject
 
