c7a97990dbd6f9bd00ade8c9ef0ac1d63795652f
Siyu Wang <cn.wangsiyu@gmail.com>
2017-11-02 11:57:14 +0800

    doc

--- PreDemObjc/Public/PREDEvent.h
+++ PreDemObjc/Public/PREDEvent.h
@@ -10,14 +10,33 @@
 #import <Foundation/Foundation.h>
 #import "PREDBaseModel.h"
 
+/**
+ * 自定义事件数据对象
+ */
 @interface PREDEvent: PREDBaseModel
 
+/**
+ * 自定义事件的名称
+ */
 @property (nonatomic, strong, readonly) NSString* name;
+
+/**
+ * 自定义事件的内容，仅支持键值对类型的内容
+ */
 @property (nonatomic, strong, readonly) NSString* content;
+
+/**
+ * 自定义事件的类型，自定义事件恒为 @"custom"
+ */
 @property (nonatomic, strong, readonly) NSString* type;
 
+/**
+ * 生成自定义事件的对象
+ *
+ * @param name 自定义事件的名称
+ * @param contentDic 自定义事件的内容，仅支持键值对类型的内容，需要传入能够被 json 化的 NSDictionary 对象
+ */
 + (instancetype)eventWithName:(NSString *)name contentDic:(NSDictionary *)contentDic;
-+ (instancetype)eventWithName:(NSString *)name type:(NSString *)type contentDic:(NSDictionary *)contentDic;
 
 @end
 
--- PreDemObjc/Public/PREDManager.h
+++ PreDemObjc/Public/PREDManager.h
@@ -10,54 +10,53 @@
 #import "PREDDefines.h"
 #import "PREDEvent.h"
 
+/**
+ * PREDManager 是 sdk 的核心类，提供 sdk 的主要对外接口
+ */
 @interface PREDManager: NSObject
 
 #pragma mark - Public Methods
 
-///-----------------------------------------------------------------------------
-/// @name Initialization
-///-----------------------------------------------------------------------------
-
-
 /**
- Initialize the manager with a PreDem app identifier.
- 
- @param appKey The app key that should be used.
- @param serviceDomain The service domain that data will be reported to or requested from.
+ * 启动 PREDManager，如果您使用的是我们的公有云产品，相关参数请通过 https://predem.qiniu.com 获取
+ *
+ * @param appKey 用于唯一标识单个 app
+ * @param serviceDomain 数据上传的服务器域名
  */
 + (void)startWithAppKey:(NSString *_Nonnull)appKey
           serviceDomain:(NSString *_Nonnull)serviceDomain
                complete:(PREDStartCompleteHandler _Nullable)complete;
 
 /**
- *  diagnose current network environment
+ *  获取当前网络环境下对指定服务器的网络诊断信息并上报
  *
- *  @param host     the end point you want this diagnose action perform with
- *  @param complete diagnose result can be retrieved from the block
+ *  @param host 需要诊断的服务器地址
+ *  @param complete 诊断结果返回
  */
 + (void)diagnose:(NSString *_Nonnull)host
         complete:(PREDNetDiagCompleteHandler _Nullable)complete;
 
+/**
+ *  上报自定义事件
+ *
+ *  @param event 需要上报的自定义事件对象
+ */
 + (void)trackEvent:(PREDEvent *_Nonnull)event;
 
-///-----------------------------------------------------------------------------
-/// @name SDK meta data
-///-----------------------------------------------------------------------------
-
 /**
- Returns the SDK Version (CFBundleShortVersionString).
+ * 返回 sdk 的版本号
  */
 + (NSString *_Nonnull)version;
 
 /**
- Returns the SDK Build (CFBundleVersion) as a string.
+ * 返回 sdk 的构建号
  */
 + (NSString *_Nonnull)build;
 
 #pragma mark - Public Properties
 
 /**
- This property is used to identify a specific user, for instance, you can assign user id to tag, so that you can use user id to search reports gathered by the sdk.
+ * 用户标签，用于标识唯一用户，例如您可以传入用户ID，我们将透传该字段，以便您可以在后台通过用户标签查找对应用户的数据
  */
 @property (class, nonnull, nonatomic, strong) NSString *tag;
 
