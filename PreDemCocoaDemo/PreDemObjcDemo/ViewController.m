72963ed5d3fc747799013d485d5ed992e3706b80
Siyu Wang <cn.wangsiyu@gmail.com>
2017-11-29 11:51:18 +0800

    bug fix

--- PreDemCocoaDemo/PreDemObjcDemo/ViewController.m
+++ PreDemCocoaDemo/PreDemObjcDemo/ViewController.m
@@ -69,13 +69,17 @@ PREDLogDelegate
 - (IBAction)viewLongPressed:(id)sender {
     UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"调试菜单" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
     NSString *actionName;
-    if (_logTextView.hidden) {
+    if (!_logTextView.delegate) {
         actionName = @"开启将 log 输出到界面";
     } else {
         actionName = @"关闭将 log 输出到界面";
     }
     [controller addAction:[UIAlertAction actionWithTitle:actionName style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        _logTextView.hidden = !_logTextView.isHidden;
+        if (!_logTextView.delegate) {
+            _logTextView.delegate = self;
+        } else {
+            _logTextView.delegate = nil;
+        }
     }]];
     
     [controller addAction:[UIAlertAction actionWithTitle:@"清空界面 log" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
