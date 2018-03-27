4286ca0b4bac1158620390a47702bd2f5c265105
Siyu Wang <cn.wangsiyu@gmail.com>
2018-03-27 16:08:42 +0800

    update

--- PreDemCocoaDemo/PreDemCocoaTests/Tests/PREDHttpTests.m
+++ PreDemCocoaDemo/PreDemCocoaTests/Tests/PREDHttpTests.m
@@ -7,17 +7,23 @@
 //
 
 #import <XCTest/XCTest.h>
-#import ""
+#import "PREDURLProtocol.h"
 
 @interface PREDHttpTests : XCTestCase
+        <
+        UIWebViewDelegate
+        >
 
 @end
 
-@implementation PREDHttpTests
+@implementation PREDHttpTests {
+    PREDPersistence *_persistence;
+}
 
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    _persistence = [[PREDPersistence alloc] init];
 }
 
 - (void)tearDown {
@@ -25,13 +31,71 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testForSharedSession {
+    [_persistence purgeAllHttpMonitor];
+    [PREDURLProtocol setPersistence:_persistence];
+    PREDURLProtocol.started = YES;
+    NSURLSession *session = [NSURLSession sharedSession];
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Download predem page"];
+    __block NSData *originalData;
+    __block NSHTTPURLResponse *originalResponse;
+    __block NSError *originalError;
+    [[session dataTaskWithURL:[NSURL URLWithString:@"http://predem.qiniu.com"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        originalData = data;
+        originalResponse = (NSHTTPURLResponse *) response;
+        originalError = error;
+        [expectation fulfill];
+    }] resume];
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+        XCTAssertNil(error, @"%@", error);
+    }];
+    NSString *filePath = [_persistence nextArchivedHttpMonitorPath];
+    XCTAssertNotEqual(filePath.length, 0);
+    NSData *data = [NSData dataWithContentsOfFile:filePath];
+    XCTAssertNotEqual(data.length, 0);
+    NSString *test = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(test);
 }
 
-- (void)testHttp {
-    NSURLSession *session = [NSURLSession sharedSession];
+- (void)testForCustomSession {
+    [_persistence purgeAllHttpMonitor];
+    [PREDURLProtocol setPersistence:_persistence];
+    PREDURLProtocol.started = YES;
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Download predem page"];
+    [[session dataTaskWithURL:[NSURL URLWithString:@"http://predem.qiniu.com"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSLog(@"%@, %@, %@", nil, response, error);
+        [expectation fulfill];
+    }] resume];
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+        XCTAssertNil(error, @"%@", error);
+    }];
+    NSString *filePath = [_persistence nextArchivedHttpMonitorPath];
+    XCTAssertNotEqual(filePath.length, 0);
+    NSData *data = [NSData dataWithContentsOfFile:filePath];
+    XCTAssertNotEqual(data.length, 0);
+    NSString *test = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(test);
+}
+
+- (void)testForHttpURLConnection {
+    [_persistence purgeAllHttpMonitor];
+    [PREDURLProtocol setPersistence:_persistence];
+    PREDURLProtocol.started = YES;
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Download predem page"];
+    [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://predem.qiniu.com"]] queue:[NSOperationQueue new] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        NSLog(@"%@, %@, %@", nil, response, connectionError);
+        [expectation fulfill];
+    }];
+    [self waitForExpectationsWithTimeout:10 handler:^(NSError *error) {
+        XCTAssertNil(error, @"%@", error);
+    }];
+    NSString *filePath = [_persistence nextArchivedHttpMonitorPath];
+    XCTAssertNotEqual(filePath.length, 0);
+    NSData *data = [NSData dataWithContentsOfFile:filePath];
+    XCTAssertNotEqual(data.length, 0);
+    NSString *test = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(test);
 }
 
 @end
