83f92046de86d44486bdb61e0a65805230dc4a18
Siyu Wang <cn.wangsiyu@gmail.com>
2018-03-27 14:08:43 +0800

    update

--- PreDemCocoa/HttpMonitor/PREDURLProtocol.m
+++ PreDemCocoa/HttpMonitor/PREDURLProtocol.m
@@ -11,25 +11,20 @@
 #import "PREDURLSessionSwizzler.h"
 #import "PREDLogger.h"
 
-#define DNSPodsHost @"119.29.29.29"
-
 static PREDPersistence *_persistence;
 static BOOL _started = NO;
+
+static NSString *kHTTPMonitorModelPropertyKey = @"PREDHTTPMonitorModel";
+static NSString *kInternalRequestPropertyKey = @"PREDInternalRequest";
+static NSString *kTaskPropertyKey = @"PREDTask";
 
 @interface PREDURLProtocol ()
         <
         NSURLSessionDataDelegate
         >
-
-@property(nonatomic, strong) NSURLSessionDataTask *task;
-@property(nonatomic, strong) NSURLResponse *response;
-@property(nonatomic, strong) PREDHTTPMonitorModel *HTTPMonitorModel;
-
 @end
 
 @implementation PREDURLProtocol
-
-@synthesize HTTPMonitorModel;
 
 + (void)setPersistence:(PREDPersistence *)persistence {
     _persistence = persistence;
@@ -64,33 +59,41 @@ static BOOL _started = NO;
 }
 
 + (BOOL)canInitWithRequest:(NSURLRequest *)request {
-    if (![request.URL.scheme isEqualToString:@"http"] &&
-            ![request.URL.scheme isEqualToString:@"https"]) {
-        return NO;
-    }
-
-    // SDK主动发送的数据
-    if ([NSURLProtocol propertyForKey:@"PREDInternalRequest" inRequest:request]) {
-        return NO;
-    }
-
-    return YES;
+    return ![NSURLProtocol propertyForKey:kInternalRequestPropertyKey inRequest:request] &&
+            ([request.URL.scheme isEqualToString:@"http"] ||
+                    [request.URL.scheme isEqualToString:@"https"]);
 }
 
 + (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request {
     NSMutableURLRequest *mutableRequest = [request mutableCopy];
-    if (mutableRequest.URL.path.length == 0) {
-        mutableRequest.URL = [NSURL URLWithString:@"/" relativeToURL:mutableRequest.URL];
-    }
+    PREDHTTPMonitorModel *HTTPMonitorModel = [[PREDHTTPMonitorModel alloc] init];
+    [NSURLProtocol setProperty:HTTPMonitorModel
+                        forKey:kHTTPMonitorModelPropertyKey
+                     inRequest:mutableRequest];
     [NSURLProtocol setProperty:@YES
-                        forKey:@"PREDInternalRequest"
+                        forKey:kInternalRequestPropertyKey
                      inRequest:mutableRequest];
-    [NSURLProtocol setProperty:mutableRequest.URL
-                        forKey:@"PREDOriginalURL"
-                     inRequest:mutableRequest];
+
+    if (request.URL.port) {
+        HTTPMonitorModel.domain = [NSString stringWithFormat:@"%@:%@", HTTPMonitorModel.domain, request.URL.port];
+    } else {
+        HTTPMonitorModel.domain = request.URL.host;
+    }
+    // 统一根目录型 path 格式
+    NSString *path = request.URL.path;
+    if (path.length == 0) {
+        path = @"/";
+    }
+    if (request.URL.query.length) {
+        HTTPMonitorModel.path = [NSString stringWithFormat:@"%@?%@", path, request.URL.query];
+    } else {
+        HTTPMonitorModel.path = path;
+    }
+    HTTPMonitorModel.method = request.HTTPMethod;
+
+    // 仅 http 可使用 ip 直连
     if ([request.URL.scheme isEqualToString:@"http"]) {
         NSMutableArray *resolvers = [[NSMutableArray alloc] init];
-        [resolvers addObject:[[QNResolver alloc] initWithAddress:DNSPodsHost]];
         [resolvers addObject:[QNResolver systemResolver]];
         QNDnsManager *dns = [[QNDnsManager alloc] init:resolvers networkInfo:[QNNetworkInfo normal]];
         NSTimeInterval dnsStartTime = [[NSDate date] timeIntervalSince1970];
@@ -99,50 +102,37 @@ static BOOL _started = NO;
             return mutableRequest;
         }
         NSTimeInterval dnsEndTime = [[NSDate date] timeIntervalSince1970];
+
+        // 判定解析出的 IP 格式是否正确
         NSError *err;
         NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^[0-9]+\\.[0-9]+\\.[0-9]+" options:0 error:&err];
+        NSAssert(err == nil, @"invalid regex %@", err);
         NSInteger number = [regex numberOfMatchesInString:replacedURL.host options:0 range:NSMakeRange(0, [replacedURL.host length])];
         if (number == 0) {
             return mutableRequest;
         }
-        [NSURLProtocol setProperty:@((dnsEndTime - dnsStartTime) * 1000)
-                            forKey:@"PREDDNSTime"
-                         inRequest:mutableRequest];
-        [NSURLProtocol setProperty:replacedURL.host
-                            forKey:@"PREDHostIP"
-                         inRequest:mutableRequest];
-        [mutableRequest setValue:request.URL.host forHTTPHeaderField:@"Host"];
+        HTTPMonitorModel.dns_time = (NSUInteger) ((dnsEndTime - dnsStartTime) * 1000);
         mutableRequest.URL = replacedURL;
+        HTTPMonitorModel.host_ip = replacedURL.host;
     }
     return mutableRequest;
 }
 
 - (void)startLoading {
+    PREDHTTPMonitorModel *HTTPMonitorModel = [NSURLProtocol propertyForKey:kHTTPMonitorModelPropertyKey inRequest:self.request];
+    HTTPMonitorModel.start_timestamp = (UInt64) ([[NSDate date] timeIntervalSince1970] * 1000);
+    HTTPMonitorModel.end_timestamp = (UInt64) ([[NSDate date] timeIntervalSince1970] * 1000);
     NSURLSessionConfiguration *sessionConfig = NSURLSessionConfiguration.ephemeralSessionConfiguration;
     NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig delegate:self delegateQueue:[NSOperationQueue new]];
-    self.task = [session dataTaskWithRequest:self.request];
-    [self.task resume];
+    NSURLSessionTask *task = [session dataTaskWithRequest:self.request];
+    [task resume];
+    [NSURLProtocol setProperty:task forKey:kTaskPropertyKey inRequest:(NSMutableURLRequest *) self.request];
     [session finishTasksAndInvalidate];
-
-    HTTPMonitorModel = [[PREDHTTPMonitorModel alloc] init];
-    NSURL *originURL = [NSURLProtocol propertyForKey:@"PREDOriginalURL" inRequest:self.request];
-    HTTPMonitorModel.domain = originURL.host;
-    if (originURL.port) {
-        HTTPMonitorModel.domain = [NSString stringWithFormat:@"%@:%@", HTTPMonitorModel.domain, originURL.port];
-    }
-    HTTPMonitorModel.path = originURL.path;
-    if (originURL.query.length) {
-        HTTPMonitorModel.path = [NSString stringWithFormat:@"%@?%@", HTTPMonitorModel.path, originURL.query];
-    }
-    HTTPMonitorModel.method = self.request.HTTPMethod;
-    HTTPMonitorModel.host_ip = [NSURLProtocol propertyForKey:@"PREDHostIP" inRequest:self.request];
-    HTTPMonitorModel.start_timestamp = [[NSDate date] timeIntervalSince1970] * 1000;
-    HTTPMonitorModel.end_timestamp = [[NSDate date] timeIntervalSince1970] * 1000;
-    HTTPMonitorModel.dns_time = [[NSURLProtocol propertyForKey:@"PREDDNSTime" inRequest:self.request] unsignedIntegerValue];
 }
 
 - (void)stopLoading {
-    [self.task cancel];
+    NSURLSessionTask *task = [NSURLProtocol propertyForKey:kTaskPropertyKey inRequest:self.request];
+    [task cancel];
 }
 
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask willCacheResponse:(NSCachedURLResponse *)proposedResponse completionHandler:(void (^)(NSCachedURLResponse *_Nullable))completionHandler {
@@ -152,18 +142,21 @@ static BOOL _started = NO;
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler {
     [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
     completionHandler(NSURLSessionResponseAllow);
-    HTTPMonitorModel.response_time_stamp = [[NSDate date] timeIntervalSince1970] * 1000;
+    PREDHTTPMonitorModel *HTTPMonitorModel = [NSURLProtocol propertyForKey:kHTTPMonitorModelPropertyKey inRequest:self.request];
+    HTTPMonitorModel.response_time_stamp = (UInt64) ([[NSDate date] timeIntervalSince1970] * 1000);
     HTTPMonitorModel.status_code = ((NSHTTPURLResponse *) response).statusCode;
 }
 
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask
     didReceiveData:(NSData *)data {
     [self.client URLProtocol:self didLoadData:data];
-    HTTPMonitorModel.end_timestamp = [[NSDate date] timeIntervalSince1970] * 1000;
+    PREDHTTPMonitorModel *HTTPMonitorModel = [NSURLProtocol propertyForKey:kHTTPMonitorModelPropertyKey inRequest:self.request];
+    HTTPMonitorModel.end_timestamp = (UInt64) ([[NSDate date] timeIntervalSince1970] * 1000);
     HTTPMonitorModel.data_length += data.length;
 }
 
 - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
+    PREDHTTPMonitorModel *HTTPMonitorModel = [NSURLProtocol propertyForKey:kHTTPMonitorModelPropertyKey inRequest:self.request];
     if (error) {
         [self.client URLProtocol:self didFailWithError:error];
         HTTPMonitorModel.network_error_code = error.code;
---
+++ PreDemCocoaDemo/PreDemCocoaTests/Tests/PREDHttpTests.m
@@ -0,0 +1,37 @@
+//
+//  PREDCrashTests.m
+//  PreDemObjcTests
+//
+//  Created by 王思宇 on 29/09/2017.
+//  Copyright © 2017 pre-engineering. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import ""
+
+@interface PREDHttpTests : XCTestCase
+
+@end
+
+@implementation PREDHttpTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testHttp {
+    NSURLSession *session = [NSURLSession sharedSession];
+}
+
+@end
